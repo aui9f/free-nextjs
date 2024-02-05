61d6fbd61ad681c1db587a1ac63ca85f8a9bc6c1
sjpark89 <sjpark@inthecore.co.kr>
2024-02-05 12:33:03 +0900

    서버사이드 조회시 백엔드에서 서버의응답을 기다렸다가 출력

---
+++ app/movies/loading.tsx
@@ -0,0 +1,3 @@
+export default function MovieLoading(){
+    return <p>Movie Loading..</p>
+}
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,11 @@
-export default function Movies (){
-    return <><p>Movies</p></>
+const URL = `https://nomad-movies.nomadcoders.workers.dev/movies`
+async function movieData(){
+    // await new Promise(res=>setTimeout(res, 10000)) 
+    return await (await fetch(URL)).json();
+}
+export default async function  Movies (){
+    const movies = await movieData()
+    return <div>
+    {JSON.stringify(movies)}
+    </div>
 }
