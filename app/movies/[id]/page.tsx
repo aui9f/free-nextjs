2de8ea6b5a32ca49e36bef3a4ae92687c8929a62
sjpark89 <sjpark@inthecore.co.kr>
2024-02-06 15:52:45 +0900

    부분별로 로딩수정

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,16 +1,8 @@
-
-
-// const getMovie  = async(id: string) => await (await fetch(`${URL}/${id}`)).json();
-// const getVideos = async (id: string) => await (await fetch(`${URL}/${id}/videos`)).json();
-
 import { Suspense } from "react";
 import Infor from "../../components/movies/infor";
 import MovieVidoe from "../../components/movies/video";
 
-
-
 export default async function Movie ({params:{id}}:{params:{id: string}}){
-    // const [info, video] = await Promise.all([getMovie(id), getVideos(id)]);
     return <>
         <p>Movie</p>
         <Suspense fallback={<p>Movie Video Data Loading..</p>}>
